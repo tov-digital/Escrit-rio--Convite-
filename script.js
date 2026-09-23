014c51c8781f935f969e4925943112cc61f942d9
Gabriel Igreja <gdaigreja@gmail.com>
2026-09-23 18:09:43 -0300

    feat: add book image and update WhatsApp invitation text

--- script.js
+++ script.js
@@ -313,7 +313,7 @@ END:VCALENDAR`;
         // WhatsApp Share Link
         const shareWspBtn = document.getElementById('shareWspBtn');
         if (shareWspBtn) {
-            const shareText = encodeURIComponent(`Olá! Gostaria de te convidar para o lançamento do livro *Devedor Contumaz* por escrito pelo *Dr. Filipe R. Carvalho*.\n\n📅 Data: 22/10/2026 às 18h\n📍 Local: Auditório da ACIRP \n\nConfirme sua presença em: \n\nconvite.advocaciafilipecarvalho.com.br`);
+            const shareText = encodeURIComponent(`Olá! Tudo bom?\n\nGostaria de te convidar para o lançamento do livro *Devedor Contumaz* escrito pelo *Dr. Filipe R. Carvalho*.\n\nSerá uma noite de confraternização e autógrafos, e sua participação é imprescindível.\n\n📅 Data: 22/10/2026 às 18h\n📍 Local: Auditório da ACIRP \n\nConfirme sua presença em: \n\nconvite.advocaciafilipecarvalho.com.br`);
             shareWspBtn.href = `https://api.whatsapp.com/send?text=${shareText}`;
         }
     }
